1d439bdba6abce9c1dd4dac102a8e8b6549739f6
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-14 10:39:23 -0300

    add root volume

--- 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet_com_security_group_sem_internet_gateway/main.tf
+++ 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet_com_security_group_sem_internet_gateway/main.tf
@@ -9,6 +9,14 @@ resource "aws_instance" "web" {
   subnet_id               =  aws_subnet.my_subnet.id # vincula a subnet direto e gera o IP automático
   private_ip              = "10.16.0.101"
   # vpc_security_group_ids  = ["${aws_security_group.allow_ssh1.id}"]
+  
+  root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true
+  }
 
   tags = {
     Name = "Maquina para testar VPC - carol"
@@ -22,6 +30,14 @@ resource "aws_instance" "web2" {
   subnet_id               =  aws_subnet.my_subnet_b.id # vincula a subnet direto e gera o IP automático
   private_ip              = "10.16.1.100"
   # vpc_security_group_ids  = ["${aws_security_group.allow_ssh1.id}"]
+  
+  root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true
+  }
 
   tags = {
     Name = "Maquina2 para testar VPC - carol"
