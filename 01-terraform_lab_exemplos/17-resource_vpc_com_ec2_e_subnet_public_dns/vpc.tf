303ad166caa0816ebdd0f9c694d9c77e5e42f837
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-14 11:21:35 -0300

    update informações da minha vpc

--- 01-terraform_lab_exemplos/17-resource_vpc_com_ec2_e_subnet_public_dns/vpc.tf
+++ 01-terraform_lab_exemplos/17-resource_vpc_com_ec2_e_subnet_public_dns/vpc.tf
@@ -1,27 +1,27 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.17.0.0/16"
-  enable_dns_hostnames = true
+  cidr_block = "10.16.0.0/22"
 
   tags = {
-    Name = "TerraformVPCPublicSubnet"
+    Name = "tf-lab-carol-vpc"
   }
 }
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.17.0.0/16"
+  cidr_block        = "10.16.0.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "tf-lab-danilo-subnet"
+    Name = "tf-lab-carol-subnet"
   }
 }
+
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "aws_internet_gateway_terraform"
+    Name = "aws_internet_gateway_terraform-carol"
   }
 }
 
@@ -47,7 +47,7 @@ resource "aws_route_table" "rt_terraform" {
   ]
 
   tags = {
-    Name = "route_table_terraform"
+    Name = "route_table_terraform-carol"
   }
 }
 
