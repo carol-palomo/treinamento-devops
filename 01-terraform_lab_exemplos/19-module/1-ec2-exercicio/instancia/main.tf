6354e5c863bf5907f7a3d429108b3d008e9ff812
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-14 14:20:04 -0300

    add ami, subnet e root block

--- 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/instancia/main.tf
+++ 01-terraform_lab_exemplos/19-module/1-ec2-exercicio/instancia/main.tf
@@ -5,7 +5,17 @@ terraform {
 resource "aws_instance" "web" {
   ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
+  subnet_id = "subnet-05880ea9006199004"
   tags = {
     Name = "${var.nome}"
   }
+  
+ root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true
+ }
+  
 }
