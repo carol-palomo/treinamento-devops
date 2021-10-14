288ebc78a338a3352a43a129d541b3949746d480
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-14 10:28:41 -0300

    update minhas informacoes

--- 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet_com_security_group_sem_internet_gateway/vpc.tf
+++ 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet_com_security_group_sem_internet_gateway/vpc.tf
@@ -1,40 +1,37 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "10.16.0.0/22"
 
   tags = {
-    Name = "tf-lab-danilo-vpc"
+    Name = "tf-lab-carol-vpc"
   }
 }
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
+  cidr_block        = "10.16.0.0/24"
   availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
 
   tags = {
-    Name = "tf-lab-danilo-subnet"
+    Name = "tf-lab-carol-subnet_a"
   }
 }
 
 resource "aws_subnet" "my_subnet_b" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.20.0/24"
+  cidr_block        = "10.16.1.0/24"
   availability_zone = "us-east-1b"
-  map_public_ip_on_launch = true
 
   tags = {
-    Name = "tf-lab-danilo-subnet_b"
+    Name = "tf-lab-carol-subnet_b"
   }
 }
 
 resource "aws_subnet" "my_subnet_c" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.30.0/24"
+  cidr_block        = "10.16.2.0/24"
   availability_zone = "us-east-1c"
-  map_public_ip_on_launch = true
 
   tags = {
-    Name = "tf-lab-danilo-subnet_c"
+    Name = "tf-lab-carol-subnet_c"
   }
 }
