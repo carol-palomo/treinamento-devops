afbf9103b7fb3bb24b11e2c8bb006c1199af0230
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-14 11:24:12 -0300

    update key, ip e root block

--- 01-terraform_lab_exemplos/17-resource_vpc_com_ec2_e_subnet_public_dns/main.tf
+++ 01-terraform_lab_exemplos/17-resource_vpc_com_ec2_e_subnet_public_dns/main.tf
@@ -5,15 +5,23 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami                     = data.aws_ami.ubuntu.id
   instance_type           = "t3.micro"
-  key_name                = "Itau_treinamento" # key chave publica cadastrada na AWS 
+  key_name                = "id_rsa" # key chave publica cadastrada na AWS 
   subnet_id               =  aws_subnet.my_subnet.id # vincula a subnet direto e gera o IP automático
-  private_ip              = "172.17.0.100"
+  private_ip              = "10.16.0.101"
   vpc_security_group_ids  = [
     "${aws_security_group.allow_ssh_terraform.id}",
   ]
+  
+  root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true
+  }
 
   tags = {
-    Name = "Maquina para testar VPC do terraform"
+    Name = "Maquina para testar VPC do terraform - carol"
   }
 }
 
