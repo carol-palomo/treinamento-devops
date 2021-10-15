c92bac26b5b03b641324622ffc2b622fff27dbc7
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-15 09:48:26 -0300

    cria maquina e security group

---
+++ 02-ansible/terraform/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami                     = "ami-09e67e426f25ce0d7"
+  instance_type           = "t3.micro"
+  key_name                = "id_rsa" # key chave publica cadastrada na AWS 
+  vpc_security_group_ids  = ["${aws_security_group.allow_ssh_tf_carol}"]
+  associate_public_ip_address = true
+  
+  root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "Maquina para testar Ansible - Carol"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_tf_carol" {
+  name        = "allow_ssh_1_vpc_terraform_carol"
+  description = "Allow SSH inbound traffic criado pelo terraform VPC"
+  vpc_id = "vpc-0304dcb48c5e67fa0"
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh_tf_carol"
+  }
+}
