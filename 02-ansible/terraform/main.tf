bde1b2926a874d6317fd3f069e04ee9b13878781
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-15 16:31:31 -0300

    Update main.tf

--- 02-ansible/terraform/main.tf
+++ 02-ansible/terraform/main.tf
@@ -23,6 +23,27 @@ resource "aws_instance" "web" {
   }
 }
 
+resource "aws_instance" "web" {
+  ami                     = "ami-09e67e426f25ce0d7"
+  instance_type           = "t3.micro"
+  key_name                = "id_rsa" # key chave publica cadastrada na AWS 
+  vpc_security_group_ids  = ["${aws_security_group.allow_ssh_tf_carol.id}"]
+  subnet_id               =  "subnet-0ab487dbac2dcfa24"
+  associate_public_ip_address = true
+  
+  root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "maq_nginx_bootstrap-carol"
+  }
+}
+
 resource "aws_security_group" "allow_ssh_tf_carol" {
   name        = "allow_ssh_1_vpc_terraform_carol"
   description = "Allow SSH inbound traffic criado pelo terraform VPC"
